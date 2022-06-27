246a9424c6fc5116c69510ddd6c5cf51e3d8ea57
Zak <zaknoel@gmail.com>
2022-06-26 21:52:57 -0700

    feat(ordersList): add functionality to the edit and delete buttons (either routes to the order form with the selected order passed in props.location.state for editing, or makes a redux action call to delete the order)

--- application/src/components/view-orders/ordersList.js
+++ application/src/components/view-orders/ordersList.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { deleteOrder } from '../../redux/actions/orderActions';
+
+const mapActionsToProps = dispatch => ({
+    commenceDeleteOrder(order) {
+        dispatch(deleteOrder(order._id));
+    }
+})
 
 const OrdersList = (props) => {
+    let history = useHistory();
+
     const { orders } = props;
     if (!props || !props.orders || !props.orders.length) return (
         <div className="empty-orders">
             <h2>There are no orders to display</h2>
         </div>
     );
+
+    const promptEditForm = (order) => {
+        history.push({
+            pathname: '/order',
+            state: { order }
+        });
+    }
+
+    const deleteOrder = (order) => {
+       props.commenceDeleteOrder(order);
+    }
 
     return orders.map(order => {
         const createdDate = new Date(order.createdAt);
@@ -21,12 +43,12 @@ const OrdersList = (props) => {
                     <p>Quantity: {order.quantity}</p>
                 </div>
                 <div className="col-md-4 view-order-right-col">
-                    <button className="btn btn-success">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+                    <button className="btn btn-success" onClick={() => promptEditForm(order)}>Edit</button>
+                    <button className="btn btn-danger" onClick={() => deleteOrder(order)}>Delete</button>
                 </div>
             </div>
         );
     });
 }
 
-export default OrdersList;
+export default connect(null, mapActionsToProps)(OrdersList);
