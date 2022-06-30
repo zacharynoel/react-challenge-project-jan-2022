97378c5e03b6245520e99b5bdee16fa6d32b5eba
Zak <zaknoel@gmail.com>
2022-06-29 23:14:54 -0700

    feat(viewOrders): add a button to start live reload, add input field for the user to enter a duration for the live reload, and display a countdown on the page to let the user know when live reload mode has ended

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -7,8 +7,36 @@ import './viewOrders.css';
 export default function ViewOrders(props) {
     const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        fetch(`${SERVER_IP}/api/current-orders`)
+    let time = 5;
+    let countdown = 0;
+
+    const onTimeChange = (event) => {
+        if (!isNaN(event.target.value)) {
+            time = event.target.value;
+        }
+    }
+
+    const startLiveReload = () => {
+        fetch(`${SERVER_IP}/api/live-mode`, {
+            method: 'POST',
+            body: JSON.stringify({
+                time
+            }),
+        })
+        .then(response => response.json())
+
+        if (!time) {
+            // if time is not set, default to 5
+            time = 5;
+        }
+
+        // set the countdown to time before starting
+        countdown = time;
+        document.getElementById("countdown").innerHTML = 'Countdown: ' + countdown;
+
+        const commenceRefresh = setInterval(() => {
+            // fetch orders
+            fetch(`${SERVER_IP}/api/current-orders`)
             .then(response => response.json())
             .then(response => {
                 if(response.success) {
@@ -17,10 +45,28 @@ export default function ViewOrders(props) {
                     console.log('Error getting orders');
                 }
             });
-    }, [])
+
+            // keep track in console each request
+            console.log('Fetched orders')
+
+            // decrement our countdown
+            countdown -= 1;
+            document.getElementById("countdown").innerHTML = 'Countdown: ' + countdown;
+        }, 1000);
+
+        setTimeout(() => {
+            // stop refreshing
+            clearInterval(commenceRefresh);
+        }, time * 1000)
+    }
 
     return (
         <Template>
+            <div>
+                <input className="live-reload-duration" type="text" id="duration" placeholder="Duration in seconds" onChange={e => onTimeChange(e)}></input>
+                <label className="live-reload-countdown" id="countdown">Countdown: {countdown}</label>
+            </div>
+            <button className="start-live-reload-btn" onClick={() => startLiveReload()}>Start Live Reload</button>
             <div className="container-fluid">
                 <OrdersList
                     orders={orders}
