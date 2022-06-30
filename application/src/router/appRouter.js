694bfe26f398e6294f07e8fddc4177134c6ad29b
Zak <zaknoel@gmail.com>
2022-06-29 20:30:40 -0700

    feat(appRouter): add new route for the /register page

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Main, Login, OrderForm, ViewOrders} from '../components';
+import Register from '../components/register/register';
 import AuthGuard from './route-guards/authGuard';
 
 const AppRouter = (props) => {
@@ -8,6 +9,7 @@ const AppRouter = (props) => {
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
       <AuthGuard>
         <Route path="/order" exact component={OrderForm} />
         <Route path="/view-orders" exact component={ViewOrders} />
